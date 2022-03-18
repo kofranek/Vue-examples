62b006271f921750cc08f28555824afbe64e369b
Hollarka <kofranek@gmail.com>
2022-03-18 03:33:39 +0100

    dialog and input

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,43 @@
 <template>
-
+  <my-input placeholder="vstup"
+            v-model:vysledek="myTitle"
+  >
+  </my-input >
+  <my-button @click="showDialog1">otevřít dialog 1</my-button>
+  <my-button @click="showDialog2">otevřít dialog 2</my-button>
+  <my-dialog v-model:show="myDialog1Visible">
+    <h2>Můj první dialog</h2>
+  </my-dialog>
+  <my-dialog v-model:show="myDialog2Visible">
+    <h2>Můj druhý dialog</h2>
+  </my-dialog>
+  <h2>{{myTitle}}</h2>
 </template>
 
 <script>
+
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      myDialog1Visible: false,
+      myDialog2Visible: false,
+      myTitle: ''
+    }
+  },
+  methods: {
+    showDialog1() {
+      console.log("showDialog")
+      this.myDialog1Visible = true
+    },
+    showDialog2() {
+      console.log("showDialog")
+      this.myDialog2Visible = true
+    }
+  }
+
+
+
 }
 </script>
 
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+      type="text"
+      :value="vysledek"
+      @input="updateInput"
+  >
+
+</template>
+
+<script>
+export default {
+  name: "MyInput",
+  props: {
+    vysledek: [String,Number],
+  },
+  methods:{
+    updateInput(event){
+      console.log('event=',event.target.value)
+      this.$emit('update:vysledek',event.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
